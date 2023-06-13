d2f3238aeaf02ab1389f6153105d33d80f71905b
marehead <marisantl@hotmail.com>
2023-06-13 18:40:39 -0300

    first commit

---
+++ estruturas de blocos/main.tf
@@ -0,0 +1,31 @@
+terraform {
+
+}
+
+provider "aws"{
+
+}
+
+resource "aws_instance" "vm1"{
+
+}
+
+data "aws_ami" "image"{
+
+}
+
+modulo "network"{
+
+}
+
+variable "vm1_ip" {
+    
+}
+
+output "vm1_ip" {
+
+}
+
+locals {
+    
+}
